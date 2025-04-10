707614ac2dfc5f62864677deecb966345a6ba4e2
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2025-04-09 22:23:38 -0400

    utilities to help dial scaling function

---
+++ test_accuracy.py
@@ -0,0 +1,29 @@
+from categories.accuracy import *
+import json
+import csv
+from tqdm import tqdm
+
+try:
+    with open("./translations.json", "r") as f:
+        translations = json.loads(f.read())
+except Exception as e:
+    print(e)
+    translations = None
+
+accuracy_scores = []
+print("Calculating accuracy scores...")
+for t in tqdm(translations):
+    acc_s = accuracy(t["german"], t["english"])
+    accuracy_scores.append(acc_s["score"])
+
+# Create a CSV file
+with open('accuracy_scores.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    # Write the header
+    writer.writerow(['German', 'English', 'Accuracy Score'])
+    # Write the data
+    print("\nWriting to CSV...")
+    for i, t in tqdm(enumerate(translations)):
+        writer.writerow([t['german'], t['english'], accuracy_scores[i]])
+
+print(f"CSV file created with {len(translations)} entries.")
